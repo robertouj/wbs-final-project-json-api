bdea8e0031dcd6a9664515dc00a26aa9af003708
Roberto Ubieto Juan <robertouj@gmail.com>
2021-06-08 10:15:06 +0200

    add users, skills and search users by skills

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
     res.render("index", {
-      title: "Time Banking Plattform - WBS Bootcamp Final Project",
+      title: "Time Time-Banking Plattform - WBS Bootcamp Final Project",
     });
 });
 
